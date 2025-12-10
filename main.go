e12cc54862eb6cdd8846d02db21159d91ea702ec
vervebot apps <vervebotapps@192.168.1.11>
2025-12-10 10:18:06 +0530

    new changes

--- main.go
+++ main.go
@@ -8,12 +8,6 @@ func main() {
 	a, b := 10, 5
 	sum := add(a, b)
 	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)
-	// Multiplication added in feat/add-mul branch
-	mul := multiply(a, b)
-	fmt.Printf("Mul: %d * %d = %d\n", a, b, mul)
-
-		div := divide(a, b)
-	fmt.Printf("Div: %d / %d = %d\n", a, b, div)
 }
 
 func add(a, b int) int {
@@ -22,8 +16,4 @@ func add(a, b int) int {
 
 func subtract(a, b int) int {
 	return a - b
-}
-
-func multiply(a, b int) int {
-	return a * b
 }
