754a7c8095eabb95be58d5be69f5f9dd1012419c
vervebot apps <vervebotapps@192.168.1.11>
2025-12-10 11:45:55 +0530

    multiple fun

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import "fmt"
 
-// Simple calculator demo to play with Git conflicts.
 func main() {
 	a, b := 10, 5
 	sum := add(a, b)
 	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)
-	// Multiplication added in feat/add-mul branch
 	mul := multiply(a, b)
 	fmt.Printf("Mul: %d * %d = %d\n", a, b, mul)
 }
