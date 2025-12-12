3ce16ea33911645a044741ab718affbd4c80e891
vervebot apps <vervebotapps@192.168.1.11>
2025-12-12 13:25:21 +0530

    add fresh main

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ func main() {
 	a, b := 10, 5
 	sum := add(a, b)
 	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)
-	mul := multiply(a, b)
-	fmt.Printf("Mul: %d * %d = %d\n", a, b, mul)
 }
 
 func add(a, b int) int {
@@ -16,8 +14,4 @@ func add(a, b int) int {
 
 func subtract(a, b int) int {
 	return a - b
-}
-// this should be added
-func multiply(a, b int) int {
-	return a * b
 }
