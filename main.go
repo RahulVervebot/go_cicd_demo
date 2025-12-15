64fc0369fb28a3534493e0978379fcfc7fb955d3
vervebot apps <vervebotapps@192.168.1.16>
2025-12-15 11:00:35 +0530

    new changes

--- main.go
+++ main.go
@@ -19,13 +19,6 @@ func subtract(a, b int) int {
 	return a - b
 }
 
-func multiple(a, b int) int {
-	if b == 0 {
-		panic("cannot divide by zero")
-	}
-	return a * b
-}
-
 func divide(a, b int) int {
 	if b == 0 {
 		panic("cannot divide by zero")
