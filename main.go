a9d9ab08ab0b1f682216b6090ddc11ce3c48c3f5
vervebot apps <vervebotapps@192.168.1.16>
2025-12-15 12:40:49 +0530

    fresh entery

--- main.go
+++ main.go
@@ -3,16 +3,11 @@ package main
 import "fmt"
 
 func main() {
+
 	a, b := 10, 5
 	sum := add(a, b)
-	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)	
-	mul := multiply(a, b)
-	fmt.Printf("Mul: %d * %d = %d\n", a, b, mul)
 	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)
-	div := divide(a, b)
-	fmt.Printf("Div: %d / %d = %d\n", a, b, div)
 }
-
 
 func add(a, b int) int {
 	return a + b
@@ -20,9 +15,4 @@ func add(a, b int) int {
 
 func subtract(a, b int) int {
 	return a - b
-}
-
-// this is multiple
-func multiply(a, b int) int {
-	return a * b
 }
