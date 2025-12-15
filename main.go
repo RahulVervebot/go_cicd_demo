46d0f8bb733f4b7e750ce9396b9edd73ed927545
vervebot apps <vervebotapps@192.168.1.16>
2025-12-15 10:59:49 +0530

    new mul

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import "fmt"
 func main() {
 	a, b := 10, 5
 	sum := add(a, b)
-	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)
-}
+	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)	
+	mul := multiply(a, b)
+	fmt.Printf("Mul: %d * %d = %d\n", a, b, mul)}
 
 func add(a, b int) int {
 	return a + b
@@ -20,4 +21,3 @@ func subtract(a, b int) int {
 func multiply(a, b int) int {
 	return a * b
 }
-
