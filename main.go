21f5e31a90b591af9e19f69ec1853a9df4766bb6
vervebot apps <therahulsinghshekhwat@gmail.com>
2025-12-16 10:12:50 +0530

    new test file

--- main.go
+++ main.go
@@ -3,21 +3,29 @@ package main
 import "fmt"
 
 func main() {
-
 	a, b := 10, 5
 	sum := add(a, b)
 	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)
+
+	div := divide(a, b)
+	fmt.Printf("Div: %d / %d = %d\n", a, b, div)
 }
 
 func add(a, b int) int {
 	return a + b
 }
+
 func subtract(a, b int) int {
 	return a - b
-
 }
-// check
+
 func multiply(a, b int) int {
 	return a * b
 }
 
+func divide(a, b int) int {
+	if b == 0 {
+		panic("cannot divide by zero")
+	}
+	return a / b
+}
