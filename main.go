204078e18eae76187c2772b3119c4c05aef107dd
vervebot apps <therahulsinghshekhawat@gmail.com>
2025-12-16 12:25:11 +0530

    new push

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	a, b := 10, 5
 	sum := add(a, b)
+	subtract := subtract(a, b)
 	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)
+  fmt.Printf("Sub: %d - %d = %d\n", a, b, subtract)
 }
 
 func add(a, b int) int {
