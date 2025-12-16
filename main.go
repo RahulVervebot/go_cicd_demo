e8b3b8cb9d4a86945f6564299a4ec679e724370d
vervebot apps <therahulsinghshekhwat@gmail.com>
2025-12-16 10:20:29 +0530

    test failed email again

--- main.go
+++ main.go
@@ -3,29 +3,16 @@ package main
 import "fmt"
 
 func main() {
+
 	a, b := 10, 5
 	sum := add(a, b)
 	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)
-
-	div := divide(a, b)
-	fmt.Printf("Div: %d / %d = %d\n", a, b, div)
+	fmt.Printf("mult: %d * %d = %d\n", a, b, sum)
 }
 
 func add(a, b int) int {
 	return a + b
 }
-
 func subtract(a, b int) int {
 	return a - b
-}
-
-func multiply(a, b int) int {
-	return a * b
-}
-
-func divide(a, b int) int {
-	if b == 0 {
-		panic("cannot divide by zero")
-	}
-	return a / b
 }
