d4a5d6427873a064eebb50c8e8ccf31af5eb9b07
vervebot apps <therahulsinghshekhawat@gmail.com>
2025-12-17 14:56:10 +0530

    check merge issue

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ func main() {
 	a, b := 10, 5
 	sum := add(a, b)
 	subtract := subtract(a, b)
+	multiply := multiply(a, b)
 	fmt.Printf("Add: %d + %d = %d\n", a, b, sum)
-
-	diff := subtract(a, b)
-	fmt.Printf("Sub: %d - %d = %d\n", a, b, diff)
+	fmt.Printf("Sub: %d - %d = %d\n", a, b, subtract)
+	fmt.Printf("Mul: %d * %d = %d\n", a, b, multiply)
 }
 
 func add(a, b int) int {
@@ -19,7 +19,7 @@ func add(a, b int) int {
 func subtract(a, b int) int {
 	return a - b
 }
-// this is new change
+
 func multiply(a, b int) int {
 	return a * b
 }
